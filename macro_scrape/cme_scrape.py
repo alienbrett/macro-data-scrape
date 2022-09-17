f36e488edc2450bd1371ad17738ff35e332a2caa
alienbrett <alienbrett648@gmail.com>
2022-09-17 11:54:00 -0400

    Moved headers to separate file

--- macro_scrape/cme_scrape.py
+++ macro_scrape/cme_scrape.py
@@ -9,19 +9,11 @@ import math
 import time
 import calendar
 
+from .headers import *
 
 cal = calendar.Calendar(firstweekday=calendar.SUNDAY)
 
 
-user_agent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Mobile Safari/537.36"
-headers = {
-    'User-Agent': user_agent,
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Connection': 'keep-alive',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US;q=0.9',
-    'Upgrade-Insecure-Requests': '1',
-}
 
 
 
